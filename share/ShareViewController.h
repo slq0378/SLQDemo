b93e410a56a85cc29939ddc36d6f7ddb2113a724
lol <lol@qq.com>
2021-07-08 17:59:22 +0800

    分享插件

---
+++ share/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  share
+//
+//  Created by song on 2021/7/8.
+//  Copyright © 2021 难说再见了. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
